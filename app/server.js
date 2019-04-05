f3ce3d9cb3f5740d0f4baddc7863021aa5d00f39
cyh0214 <cyh0214@namuplanet.com>
2019-04-05 09:29:02 +0900

    add route - v2

--- app/server.js
+++ app/server.js
@@ -11,6 +11,10 @@ router.get('/main', function (req, res) {
     res.sendFile(path.join(__dirname + '/views/main.html'));
 });
 
+router.get('/v2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/v2.html'));
+});
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
